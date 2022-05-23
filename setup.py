613a57ca8d508d6c2ec0eca0d1e8d402e5dafcb1
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2022-05-23 20:41:16 +0200

    add version number, require wheel in setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="download_tools",
-    version="",
+    version="0.0.0",
     packages=["download_tools", "download_tools.plugins"],
     url="",
     license="",
     author="Rationality Enhancement Group",
     author_email="",
     description="",
+    setup_requires=['wheel']
     install_requires=[
         "numpy",
         "pandas",
